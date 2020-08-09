d5ea199f761d4fca4518494d1ca08da63b82553b
Joshua Pattullo <joshua@pattullo.tech>
2020-08-09 16:30:22 -0600

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ *Author: Joshua Pattullo
+ *Generated on 2020-08-09
+ *License: GPL-3.0
+ */
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
